87ac69eb03b29f3fb2a2d48525110d84665234e5
RawwCod <raul.huraccahua@ucsm.edu.pe>
2022-11-04 16:50:50 -0500

    de nada

--- function/header.php
+++ function/header.php
@@ -1,4 +1,6 @@
 <header>
+    <br>
+    <br>
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
@@ -38,73 +40,3 @@
         </a>
     </div>
 </header>
-<!-- espacio entre los dos headers -->  
-<!-- <!DOCTYPE html>
-<html>
-    <head>
-    <link rel="stylesheet" href="Header.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-  </head>
-    <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-    <header class="p-3 text-bg-dark">
-
-    <div class="container">
-
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-
-        <a href="/" class="d-flex align-items-cente r mb-2 mb-lg-0 text-white text-decoration-none">
-        <img src="cato.jpeg" alt="mdo" width="30" height="30" class="rounded-circle"><use xlink:href="#"/>
-        </a>
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-
-        <ul class="menu">
-                    <li><a href="#">Inicio</a>
-                    
-                <li><a href="users.php">Pacientes</a>
-                    <ul class="submenu">
-                        <li><a href="registration.php">Registrar Paciente</a></li>
-                    </ul>
-                </li>
-                <li><a href="userscli.php">Servicios</a>
-                    <ul class="submenu">
-                 <li><a href="registrationcli.php">Listar</a></li>
-          </ul>
-                <li><a href="userSer.php">Servicios</a>
-                <ul class="submenu">
-               <li> <a href="registrationSer.php">Registrar Servicio</a></li>
-        </ul>
-        <li><a href="cliSer.php">Operaciones</a>
-              <ul class="submenu">
-               <li> <a href="registrationCliSer.php">Registrar CliSer</a></li>
-
-        </ul>
-
-          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-
-        </ul>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
-
-        </form>
-
-
-
-        <div class="text-end">
-
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-
-          <button type="button" class="btn btn-warning">Cerrar Ses</button>
-
-        </div>
-
-      </div>
-
-    </div>
-
-  </header>
-    </body>
-</html> -->
--- function/nav.php
+++ function/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-light" style="background-color: #e3f2fd; margin-bottom: 0%;">
+<!-- <nav class="navbar navbar-light" style="background-color: #e3f2fd; margin-bottom: 0%;">
     <a class="navbar-brand" id="ocultar" href="#">¡Centro Medico!</a>
     <ul class="nav navbar-nav">
         <li class="nav-item active">
@@ -11,10 +11,10 @@
             <a class="nav-link" href="#" data-toggle="modal" data-target=".bs-example-modal-sm">Doctor</a>
         </li>
     </ul>
-</nav>
+</nav> -->
 
 <!-- Small modal -->
-<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+<!-- <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
     <div class="modal-dialog modal-sm" role="document">
         <div class="modal-content">
             <div>
@@ -25,4 +25,73 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="./css/Header.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+  </head>
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <header class="p-3 text-bg-info">
+
+    <div class="container">
+
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+
+        <ul class="menu">
+                    <li><a href="#">Inicio</a>
+                    
+                <li><a href="./listar.php">Clientes</a>
+                    <ul class="submenu">
+                        <li><a href="./index.php">Registrar</a></li>
+                    </ul>
+                </li>
+                <li><a href="./listarSer.php">Servicios</a>
+                    <ul class="submenu">
+                 <li><a href="./registrarSer.php">Registrar</a></li>
+          </ul>
+                <li><a href="./operacionesCli.php">Operaciones</a>
+                <ul class="submenu">
+               <li> <a href="#">Registrar Servicio</a></li>
+        </ul>
+        <li><a href="#">Operaciones</a>
+              <ul class="submenu">
+               <li> <a href="#">Registrar CliSer</a></li>
+
+        </ul>
+
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+
+        </ul>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+          <input type="search" class="form-control form-control-primary text-bg-primary" placeholder="Search..." aria-label="Search">
+
+        </form>
+
+
+
+        <div class="text-end">
+
+          <button type="button" class="btn btn-outline-light me-2">Login</button>
+
+          <button type="button" class="btn btn-warning">Cerrar Ses</button>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </header>
+    </body>
+</html>
