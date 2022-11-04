11c22af10b0ef737c75be610bf63375130183ed7
RawwCod <raul.huraccahua@ucsm.edu.pe>
2022-11-04 18:25:09 -0500

    gaaa4

--- function/nav.php
+++ function/nav.php
@@ -14,18 +14,18 @@
 </nav> -->
 
 <!-- Small modal -->
-<!-- <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-    <div class="modal-dialog modal-sm" role="document">
-        <div class="modal-content">
-            <div>
-                <div class="thumbnail">
-                    <img src="img/doctor.jpg">
-                    <div class="caption"><p class="text-center text-uppercase">Doctor Corazon <br><spam><b>Raul Huraccahua</b></spam></p></div>
+    <!-- <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+        <div class="modal-dialog modal-sm" role="document">
+            <div class="modal-content">
+                <div>
+                    <div class="thumbnail">
+                        <img src="img/doctor.jpg">
+                        <div class="caption"><p class="text-center text-uppercase">Doctor Corazon <br><spam><b>Raul Huraccahua</b></spam></p></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div> -->
+    </div> -->
 
 
 
@@ -65,7 +65,6 @@
         <li><a href="#">Operaciones</a>
               <ul class="submenu">
                <li> <a href="#"></a></li>
-
         </ul>
 
           <li><a href="#" class="nav-link px-2 text-white">About</a></li>
